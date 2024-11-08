b064cf0d252b1709d4e081ce1e42f18831803176
brandonUre2 <145625481+brandonUre2@users.noreply.github.com>
2024-11-07 22:28:51 -0600

    Agregar archivos de proyecto.

---
+++ WorksSpacesG9/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorksSpacesG9.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.IsLoggedIn = User.Identity.IsAuthenticated;
+
+            
+            if (Session["idUsuario"] != null)
+            {
+                ViewBag.UserId = (int)Session["idUsuario"];
+            }
+            else
+            {
+                ViewBag.UserId = null; 
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Error()
+        {
+            return View();
+
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
