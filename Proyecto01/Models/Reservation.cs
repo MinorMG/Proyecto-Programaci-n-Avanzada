e3e59a4fedf2a8adf21947682a9a193f5fb4fbf1
MinorMG <minorjose2015@gmail.com>
2024-11-20 13:51:25 -0600

    Proyecto con segurida y algunos modelos
    Hace falta de integrar lo controladores

---
+++ Proyecto01/Models/Reservation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto01.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int ReservationId { get; set; }
+
+        [Required]
+        public int RoomId { get; set; } // Llave foránea a Room
+
+        [ForeignKey("RoomId")]
+        public virtual Rooms Rooms { get; set; } // Relación con Room
+
+        [ForeignKey("UserId")]
+        public virtual User user{ get; set; } // Relación con Room
+
+        [Required]
+        public DateTime ReservationDate { get; set; } // Fecha de la reserva
+
+        [Required]
+        public TimeSpan StartTime { get; set; } // Hora de inicio
+
+        [Required]
+        public TimeSpan EndTime { get; set; } // Hora de fin
+    }
+}
---
+++ Proyecto01/Models/Rooms.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto01.Models
+{
+    public class Rooms
+    {
+        [Key]
+        public int RoomId { get; set; }
+
+        [Required]
+        [StringLength(100)] // Máximo 100 caracteres
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 500)] // Capacidad entre 1 y 500 personas
+        public int Capacity { get; set; }
+
+        [Required]
+        [StringLength(200)] // Máximo 200 caracteres para ubicación
+        public string Location { get; set; }
+
+        [StringLength(500)] // Opcional, descripción de equipo disponible
+        public string Equipment { get; set; }
+
+        [Required]
+        public TimeSpan AvailableFrom { get; set; } // Ejemplo: 09:00
+
+        [Required]
+        public TimeSpan AvailableTo { get; set; } // Ejemplo: 18:00
+
+        // Relación con Reservation (1 sala tiene muchas reservas)
+        public virtual ICollection<Reservation> Reservations { get; set; }
+
+
+
+    }
+}
